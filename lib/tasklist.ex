3c8014426697bc6cdf23ab6557da9ebc5f82bb69
luc50514@gmail.com <luc50514@gmail.com>
2022-06-07 21:58:50 -0400

    Added another test to remove feature

--- lib/tasklist.ex
+++ lib/tasklist.ex
@@ -26,4 +26,37 @@ defmodule Tasklist do
     |> Stream.filter(fn {_, entry,} -> entry.date == date end)
     |> Enum.map(fn {_, entry} -> entry end)
   end
+
+
+  def getTaskListById(tasklist, entryId) do
+    case Map.fetch(tasklist.entries, entryId) do
+      :error -> {:error, tasklist}
+      {:ok, entry} -> {:ok, entry}
+    end
+  end
+  def updateTaskList(tasklist, %{} = newentry) do
+    updateTaskList(tasklist, newentry.id, fn _ -> newentry end)
+  end
+
+  def updateTaskList(tasklist, entryId, update_fn) do
+    case Map.fetch(tasklist.entries, entryId) do
+      :error ->
+        tasklist
+
+      {:ok, oldEntry} ->
+        newentry = update_fn.(oldEntry)
+
+      newentries = Map.put(
+          tasklist.entries,
+          newentry.id,
+          newentry
+        )
+
+      %Tasklist{
+          tasklist
+          | entries: newentries
+        }
+    end
+
+  end
 end
--- test/tasklist_test.exs
+++ test/tasklist_test.exs
@@ -18,4 +18,15 @@ defmodule TasklistTest do
     assert Tasklist.getTaskList(%Tasklist{entries: %{0 => %{date: ~D[2022-06-05], id: 0, value: "Ebill Added"}}, id: 1}, ~D[2022-06-05]) ==  [%{date: ~D[2022-06-05], id: 0, value: "Ebill Added"}]
   end
 
+  test "Tasklist.updateTaskList should return %Tasklist{entries: %{0 => %{date: ~D[2022-06-05], id: 0, value: Ebill Added More than 1}, id: 1}" do
+    assert Tasklist.updateTaskList(%Tasklist{entries: %{0 => %{date: ~D[2022-06-05], id: 0, value: "Ebill Added"}}, id: 1}, %{date: ~D[2022-06-05], id: 0, value: "Ebill Added More than 1"}) == %Tasklist{entries: %{0 => %{date: ~D[2022-06-05], id: 0, value: "Ebill Added More than 1"}}, id: 1}
+  end
+
+  test "Tasklist.getTaskListById should return %Tasklist{entries: %{0 => %{date: ~D[2022-06-05], id: 0, value: Ebill Added}, id: 1}" do
+    assert Tasklist.getTaskListById(%Tasklist{entries: %{0 => %{date: ~D[2022-06-05], id: 0, value: "Ebill Added"}}, id: 1}, 1) == {:error, %Tasklist{entries: %{0 => %{date: ~D[2022-06-05], id: 0, value: "Ebill Added"}}, id: 1}}
+  end
+
+  test "Tasklist.getTaskListById should return map when id is in tasklist" do
+    assert Tasklist.getTaskListById(%Tasklist{entries: %{0 => %{date: ~D[2022-06-05], id: 0, value: "Ebill Added"}}, id: 1}, 0) == {:ok, %{date: ~D[2022-06-05], id: 0, value: "Ebill Added"}}
+  end
 end
