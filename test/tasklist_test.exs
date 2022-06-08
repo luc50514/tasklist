249a062e2d9df59a0c9132fbbfeacf4e8ca3a574
luc50514@gmail.com <luc50514@gmail.com>
2022-06-08 15:48:23 -0400

    added another test for the code

--- test/tasklist_test.exs
+++ test/tasklist_test.exs
@@ -29,4 +29,19 @@ defmodule TasklistTest do
   test "Tasklist.getTaskListById should return map when id is in tasklist" do
     assert Tasklist.getTaskListById(%Tasklist{entries: %{0 => %{date: ~D[2022-06-05], id: 0, value: "Ebill Added"}}, id: 1}, 0) == {:ok, %{date: ~D[2022-06-05], id: 0, value: "Ebill Added"}}
   end
+
+  test "Tasklist.updateTaskList should update values in TaskList" do
+    task1 = Tasklist.addTaskList(%Tasklist{entries: %{}, id: 0}, %{date: ~D[2022-06-05], value: "Add Payee"})
+    task2 =Tasklist.addTaskList(task1, %{date: ~D[2022-06-05], value: "Enroll Payee"})
+    task3 = Tasklist.addTaskList(task2, %{date: ~D[2022-06-05], value: "Get Bill from provider"})
+    assert Tasklist.updateTaskList(task3, %{date: ~D[2022-06-05], id: 2, value: "Add ebill for today"}) == %Tasklist{
+      entries: %{
+        0 => %{date: ~D[2022-06-05], id: 0, value: "Add Payee"},
+        1 => %{date: ~D[2022-06-05], id: 1, value: "Enroll Payee"},
+        2 => %{date: ~D[2022-06-05], id: 2, value: "Add ebill for today"}
+      },
+      id: 3
+    }
+
+  end
 end
